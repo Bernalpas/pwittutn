fc3e10cfe34c38f8820c99fad19c1e0c0b2c4c18
Berni <bernalpas@gmail.com>
2023-06-07 17:37:11 -0300

    Clases

---
+++ clase-02/js/index.js
@@ -0,0 +1,19 @@
+let password = '1234568979'
+console.log(password);
+
+let otroPassword = document.getElementById('otroPassword').value;
+let otroPasswords = document.getElementById("otroPassword").value;
+let otrosPassword = document.getElementById(`otroPassword`).value;
+console.log(otroPassword);
+
+
+/* 
+js es igual password = otroPassword = si 
+joya! esta todo bien!!
+los password no coinciden  
+*/
+
+
+console.log("Mi password es: password: ${password} " + password);
+
+console.log(`Mi password es: ${password}`); 
