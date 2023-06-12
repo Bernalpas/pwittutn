2e9af38be13f8ddb83a0abec38a6ff17a945ec0c
Berni <bernalpas@gmail.com>
2023-06-12 17:20:11 -0300

    agregamos clase 07 JS

---
+++ clase-07-js/js/index.js
@@ -0,0 +1,141 @@
+
+console.log('3. Estoy en el archivo externo');
+
+/* 
+Comentarios 
+múltiples
+*/
+
+
+//console.log('Esto es un comentario de una línea');
+
+//Palabras reservadas: dar indicaciones de tareas o funciones al lenguaje de programación
+/* var
+let 
+for
+function
+if 
+*/
+
+/* Casa
+casa
+CAsa
+caSA
+CASA 
+*/
+
+//variables. espacio de memoria para guardar datos
+//declaración de una variable
+var nombre; //no es obligatorio el ;
+
+//asignación de una variable
+// = : es un operador de asignación
+nombre = "Pepe";
+
+//recuperar la info 
+console.log(nombre);
+
+var apellido = 'Pérez';//js es flexible 
+
+console.log(apellido);
+
+//Declaramos y asignamos otra variable
+provincia = 'Mendoza';
+
+console.log(provincia);
+
+//concatenar datos
+console.log('Mi nombre es: ' + nombre + ' y mi apellido es: ' + apellido + " y vivo en la provincia de " + provincia);
+
+console.log('=============================================================');
+
+//template string: interpolar variables con strings
+console.log(`Mi nombre es ${nombre} y mi apellido es ${apellido} y vivo en la provincia de ${provincia}`);
+
+
+let pais = 'Argentina';//con let puedo declarar una sóla vez
+
+var apellido = 'Gómez'
+
+console.log('Mi nombre es: ' + nombre + ' y mi apellido es: ' + apellido + " y vivo en la provincia de " + provincia);
+
+
+//let pais = 'Brasil';
+
+pais = 'Uruguay';
+
+pais = '1500';
+dos = 2000
+
+resultado = pais + dos
+
+console.log(resultado);
+
+
+console.log('=============================================================');
+
+//constante: no cambia
+const dni = 123456789;
+
+//const apellido = 'Brasil';
+
+console.log(apellido);
+
+//dni = 11111111111;
+
+
+console.log('=============================================================');
+console.log('=============================================================');
+
+//operadores matemáticos: + - * / % 
+let numUno = 1000;
+let numDos = 5000;
+
+resultado = numUno + numDos;
+console.log(`El resultado de la suma es ${resultado}`);
+
+console.log('=============================================================');
+
+resultado = numDos - numUno;
+console.log(resultado);
+
+console.log('=============================================================');
+
+resultado = numUno * numDos;
+console.log(resultado);
+
+console.log('=============================================================');
+
+resultado = numDos / numUno;//resultado de la división
+console.log(resultado);
+
+console.log('=============================================================');
+
+resultado = numDos % numUno;//resto de la división
+console.log(resultado);
+
+//booleanos
+let verdadero = true;
+let falso = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
